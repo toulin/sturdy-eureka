5dbfd60252445d423dd09b8aa19fd9b47bd99b63
toulin <toulin@cmoney.com.tw>
2017-09-16 00:20:34 +0800

    Create NetOffice Test For VS 2017

---
+++ NetOffice/MyTry_Addin_VS2017/NetOffice_Try_Addin/NetOffice_Try_Addin/AddinTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetOffice_Try_Addin
+{
+    public class AddinTest
+    {
+
+    }
+}
