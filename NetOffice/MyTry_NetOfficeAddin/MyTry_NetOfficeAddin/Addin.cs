3aa9bec74756e8ed44174186263781df15cab08e
Toulin <toulin@cmoney.com.tw>
2017-09-15 19:16:10 +0800

    新增NetOffice測試

---
+++ NetOffice/MyTry_NetOfficeAddin/MyTry_NetOfficeAddin/Addin.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NetOffice;
+using NetOffice.Tools;
+using Excel = NetOffice.ExcelApi;
+using NetOffice.ExcelApi.Enums;
+using NetOffice.ExcelApi.Tools;
+using Office = NetOffice.OfficeApi;
+using NetOffice.OfficeApi.Enums;
+using NetOffice.OfficeApi.Tools;
+using VBIDE = NetOffice.VBIDEApi;
+using NetOffice.VBIDEApi.Enums;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Drawing;
+
+namespace MyTry_NetOfficeAddin
+{
+    /*
+        [COMAddin("DeploymentSample", "Deployment Example Addin in C#", 3)]
+    [ProgId("DeploymentSample.Addin"), Guid("1bf185d4-76ee-46d9-b709-afd20f78943b")]
+     */
+    [COMAddin("ToulinTryNetOfficeAddin", "Assembly Description", 3), ProgId("ToulinTryNetOfficeAddin.Addin"), Guid("4EE8530A-70BA-4F03-9ADB-96D93E87DDC2")]
+    [RegistryLocation(RegistrySaveLocation.CurrentUser)]
+    public class Addin : NetOffice.ExcelApi.Tools.COMAddin, IDTExtensibility2
+    {
+
+        private static readonly string _addinOfficeRegistryKey = "Software\\Microsoft\\Office\\Excel\\AddIns\\";
+        private static readonly string _progId = "ToulinTryNetOfficeAddin.Addin";
+        private static readonly string _addinFriendlyName = "測試NetOffie取得ExcelVersion";
+        private static readonly string _addinDescription = "測試NetOffie取得ExcelVersion";
+
+        // gui elements
+        private static readonly string _toolbarName = "取得ExcelVersion_ToolbarName";
+        private static readonly string _toolbarButtonName = "取得ExcelVersion_ToolbarButtonName";
+        private static readonly string _toolbarPopupName = "取得ExcelVersion_ToolbarPopupName ";
+        private static readonly string _menuName = "取得ExcelVersion_MenuName";
+        private static readonly string _menuButtonName = "取得ExcelVersion_MenuButtonName";
+        private static readonly string _contextName = "取得ExcelVersion_ContextName";
+        private static readonly string _contextMenuButtonName = "取得ExcelVersion_ContextMenuButtonName";
+
+        Excel.Application _excelApplication;
+
+        #region IDTExtensibility2 Members
+
+        void IDTExtensibility2.OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            try
+            {
+                _excelApplication = new Excel.Application(null, Application);
+            }
+            catch (Exception exception)
+            {
+                string message = string.Format("An error occured.{0}{0}{1}", Environment.NewLine, exception.Message);
+                MessageBox.Show(message, _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void IDTExtensibility2.OnStartupComplete(ref Array custom)
+        {
+            try
+            {
+                CreateTemporaryUserInterface();
+            }
+            catch (Exception exception)
+            {
+                string message = string.Format("An error occured.{0}{0}{1}", Environment.NewLine, exception.Message);
+                MessageBox.Show(message, _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void IDTExtensibility2.OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            try
+            {
+                if (null != _excelApplication)
+                    _excelApplication.Dispose();
+            }
+            catch (Exception exception)
+            {
+                string message = string.Format("An error occured.{0}{0}{1}", Environment.NewLine, exception.Message);
+                MessageBox.Show(message, _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void IDTExtensibility2.OnAddInsUpdate(ref Array custom)
+        {
+
+        }
+
+        void IDTExtensibility2.OnBeginShutdown(ref Array custom)
+        {
+
+        }
+
+        #endregion
+
+        [RegisterErrorHandler]
+        public static void RegisterErrorHandler(RegisterErrorMethodKind methodKind, System.Exception exception)
+        {
+            MessageBox.Show("An error occurend in " + methodKind.ToString(), "ToulinTryNetOfficeAddin");
+        }
+
+
+
+        #region COM Register Functions
+
+        [ComRegisterFunctionAttribute]
+        public static void RegisterFunction(Type type)
+        {
+            try
+            {
+                // add codebase value
+                Assembly thisAssembly = Assembly.GetAssembly(typeof(Addin));
+                RegistryKey key = Registry.ClassesRoot.CreateSubKey("CLSID\\{" + type.GUID.ToString().ToUpper() + "}\\InprocServer32\\1.0.0.0");
+                key.SetValue("CodeBase", thisAssembly.CodeBase);
+                key.Close();
+
+                Registry.ClassesRoot.CreateSubKey(@"CLSID\{" + type.GUID.ToString().ToUpper() + @"}\Programmable");
+
+                // add bypass key
+                // http://support.microsoft.com/kb/948461
+                key = Registry.ClassesRoot.CreateSubKey("Interface\\{000C0601-0000-0000-C000-000000000046}");
+                string defaultValue = key.GetValue("") as string;
+                if (null == defaultValue)
+                    key.SetValue("", "Office .NET Framework Lockback Bypass Key");
+                key.Close();
+
+                // register addin in Excel
+                RegistryKey regKeyExcel = Registry.CurrentUser.CreateSubKey(_addinOfficeRegistryKey + _progId);
+                regKeyExcel.SetValue("LoadBehavior", Convert.ToInt32(3));
+                regKeyExcel.SetValue("FriendlyName", _addinFriendlyName);
+                regKeyExcel.SetValue("Description", _addinDescription);
+                regKeyExcel.Close();
+            }
+            catch (Exception ex)
+            {
+                string details = string.Format("{1}{1}Details:{1}{1}{0}", ex.Message, Environment.NewLine);
+                MessageBox.Show("An error occured." + details, "Register " + _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void UnregisterFunction(Type type)
+        {
+            try
+            {
+                // unregister addin
+                Registry.ClassesRoot.DeleteSubKey(@"CLSID\{" + type.GUID.ToString().ToUpper() + @"}\Programmable", false);
+
+                // unregister addin in office
+                Registry.CurrentUser.DeleteSubKey(_addinOfficeRegistryKey + _progId, false);
+
+            }
+            catch (Exception throwedException)
+            {
+                string details = string.Format("{1}{1}Details:{1}{1}{0}", throwedException.Message, Environment.NewLine);
+                MessageBox.Show("An error occured." + details, "Unregister" + _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        private void CreateTemporaryUserInterface()
+        {
+            /*
+            // How to: Add Commands to Shortcut Menus in Excel
+            // http://msdn.microsoft.com/en-us/library/0batekf4.aspx            
+            */
+
+            /* create commandbar */
+            Office.CommandBar commandBar = _excelApplication.CommandBars.Add(_toolbarName, MsoBarPosition.msoBarTop, System.Type.Missing, true);
+            commandBar.Visible = true;
+
+            // add popup to commandbar
+            Office.CommandBarPopup commandBarPop = (Office.CommandBarPopup)commandBar.Controls.Add(MsoControlType.msoControlPopup, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarPop.Caption = _toolbarPopupName;
+            commandBarPop.Tag = _toolbarPopupName;
+
+            // add a button to the popup
+            Office.CommandBarButton commandBarBtn = (Office.CommandBarButton)commandBarPop.Controls.Add(MsoControlType.msoControlButton, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarBtn.Style = MsoButtonStyle.msoButtonIconAndCaption;
+            commandBarBtn.FaceId = 9;
+            commandBarBtn.Caption = _toolbarButtonName;
+            commandBarBtn.Tag = _toolbarButtonName;
+            commandBarBtn.ClickEvent += new NetOffice.OfficeApi.CommandBarButton_ClickEventHandler(commandBarBtn_ClickEvent);
+
+            /* create menu */
+            commandBar = _excelApplication.CommandBars["Worksheet Menu Bar"];
+
+            // add popup to menu bar
+            commandBarPop = (Office.CommandBarPopup)commandBar.Controls.Add(MsoControlType.msoControlPopup, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarPop.Caption = _menuName;
+            commandBarPop.Tag = _menuName;
+
+            // add a button to the popup
+            commandBarBtn = (Office.CommandBarButton)commandBarPop.Controls.Add(MsoControlType.msoControlButton, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarBtn.Style = MsoButtonStyle.msoButtonIconAndCaption;
+            commandBarBtn.FaceId = 9;
+            commandBarBtn.Caption = _menuButtonName;
+            commandBarBtn.Tag = _menuButtonName;
+            commandBarBtn.ClickEvent += new NetOffice.OfficeApi.CommandBarButton_ClickEventHandler(commandBarBtn_ClickEvent);
+
+            /* create context menu */
+            commandBarPop = (Office.CommandBarPopup)_excelApplication.CommandBars["Cell"].Controls.Add(MsoControlType.msoControlPopup, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarPop.Caption = _contextName;
+            commandBarPop.Tag = _contextName;
+
+            // add a button to the popup
+            commandBarBtn = (Office.CommandBarButton)commandBarPop.Controls.Add(MsoControlType.msoControlButton, System.Type.Missing, System.Type.Missing, System.Type.Missing, true);
+            commandBarBtn.Style = MsoButtonStyle.msoButtonIconAndCaption;
+            commandBarBtn.Caption = _contextMenuButtonName;
+            commandBarBtn.Tag = _contextMenuButtonName;
+            commandBarBtn.FaceId = 9;
+            commandBarBtn.ClickEvent += new NetOffice.OfficeApi.CommandBarButton_ClickEventHandler(commandBarBtn_ClickEvent);
+        }
+
+        //private void RemoveUserInterface()
+        //{
+        //   // _excelApplication.CommandBars("Cell").Reset();
+        //}
+
+        #region UI Trigger
+
+        /// <summary>
+        /// Click event trigger from created buttons. incoming call comes from excel application thread.
+        /// </summary>
+        /// <param name="Ctrl"></param>
+        /// <param name="CancelDefault"></param>
+        private void commandBarBtn_ClickEvent(NetOffice.OfficeApi.CommandBarButton Ctrl, ref bool CancelDefault)
+        {
+            try
+            {
+                Excel.Worksheet workSheet = (Excel.Worksheet)_excelApplication.ActiveWorkbook.ActiveSheet;
+                workSheet.Cells[1, 1].Value = "ExcelVersion";
+                workSheet.Cells[1, 2].Value = _excelApplication.Version;
+
+                // create a utils instance, not need for but helpful to keep the lines of code low
+                NetOffice.ExcelApi.Tools.CommonUtils utils = new NetOffice.ExcelApi.Tools.CommonUtils(_excelApplication);
+
+                // draw back color and perform the BorderAround method
+                workSheet.Range("$B2:$B5").Interior.Color = utils.Color.ToDouble(Color.DarkGreen);
+                workSheet.Range("$B2:$B5").BorderAround(XlLineStyle.xlContinuous, NetOffice.ExcelApi.Enums.XlBorderWeight.xlMedium, NetOffice.ExcelApi.Enums.XlColorIndex.xlColorIndexAutomatic);
+
+                MessageBox.Show(_excelApplication.Version, "ExcelVersion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Ctrl.Dispose();
+            }
+            catch (Exception exception)
+            {
+                string message = string.Format("An error occured.{0}{0}{1}", Environment.NewLine, exception.Message);
+                MessageBox.Show(message, _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+    }
+}
+
---
+++ NetOffice/MyTry_NetOfficeAddin/MyTry_NetOffice_Installer/Installer1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Reflection;
+
+namespace MyTry_NetOffice_Installer
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+            //System.Diagnostics.Debugger.Launch();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            Regasm(false);
+
+            //string targetDir = this.Context.Parameters["targetDir"];
+            ////覆寫範本
+            //prjoFileReplace repFile = new prjoFileReplace(targetDir,
+            //"TemplatePPT", @"Output\WordTemplate",
+            //@"Replace\PPT", @"Replace\Word");
+            //repFile.ReplaceFile();
+
+            ////***重建捷徑
+            //string pgMenuFolder = Path.Combine(this.Context.Parameters["programMenuFolder"], "群益研究報告管理系統"); //所在程式集目錄
+            ////桌面
+            //CreateShortcut(this.Context.Parameters["desktopFolder"],
+            //                "群益研究報告管理系統", Path.Combine(targetDir, "CMProjectLogin.exe"), "群益研究報告管理系統");
+            ////程式集
+            //CreateShortcut(pgMenuFolder,
+            //                "群益研究報告管理系統", Path.Combine(targetDir, "CMProjectLogin.exe"), "群益研究報告管理系統");
+            //CreateShortcut(pgMenuFolder,
+            //                "帳號與股票權限管理工具", Path.Combine(targetDir, "CMoney2.ActManager.exe"), "帳號與股票權限管理工具");
+            //CreateShortcut(pgMenuFolder,
+            //                "群益研究報告產出模組", Path.Combine(targetDir, @"Output\CMoneyTradex.exe"), "群益研究報告產出模組");  
+            //CreateShortcut(pgMenuFolder,
+            //                "Word範本資料夾", Path.Combine(targetDir, @"Output\WordTemplate"), "Word範本資料夾");            
+            //CreateShortcut(pgMenuFolder,
+            //                "研究報告輸出資料夾", Path.Combine(targetDir, @"Output\WordOutput"), "研究報告輸出資料夾");            
+
+            ////***
+
+
+            //this.Committed += new InstallEventHandler(Installer1_Committed);
+            //string allUsersString = this.Context.Parameters["allUsers"];            
+            //OpenCloseFile();
+        }
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            base.OnAfterUninstall(savedState);
+            Regasm(true);
+            // string targetDir = this.Context.Parameters["targetDir"];
+
+
+        }
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            OnAfterUninstall(savedState);
+
+            //string targetDir = this.Context.Parameters["targetDir"];
+
+        }
+
+        //void Installer1_Committed(object sender, InstallEventArgs e)
+        //{
+        //    OpenCloseFile();
+        //}
+
+        /// <summary>
+        /// 註冊與反註冊的動作
+        /// </summary>
+        /// <param name="mainName">dll主檔名</param>
+        private void Regasm(bool unRegister)
+        {
+            string targetDir = this.Context.Parameters["targetDir"];
+            string dll = Path.Combine(targetDir, "MyTry_NetOfficeAddin.dll");
+            string tlb = Path.Combine(targetDir, "MyTry_NetOfficeAddin.tlb");
+            string regasmPath = Path.Combine(Environment.GetEnvironmentVariable("windir"), @"Microsoft.NET\Framework\v3.5\regasm.exe");
+            if (File.Exists(regasmPath) == false)
+            { regasmPath = Path.Combine(Environment.GetEnvironmentVariable("windir"), @"Microsoft.NET\Framework\v4.0.30319\regasm.exe"); }
+
+            string parame = string.Format(@"""{0}"" /tlb: ""{1}"" /codebase", dll, tlb);
+            if (unRegister)
+                parame = string.Format(@"-u ""{0}"" /codebase", regasmPath, dll);
+            //parame = string.Format(@"/unregister ""{0}""", regasmPath, dll);
+
+            try
+            {
+                if (File.Exists(regasmPath) == false)
+                    throw new Exception("找不到檔案,請重新安裝.Net framework 4.0\r\n" + regasmPath);
+                Process proc = new Process();
+                proc.StartInfo.FileName = regasmPath;
+                proc.StartInfo.Arguments = parame;
+                proc.Start();
+            }
+            catch (Exception err)
+            { throw new Exception(string.Format("{0} \r\n{1}\r\n{2}", err.Message, regasmPath, parame)); }
+        }
+        #region COM Register Functions
+
+        //public  void RegisterFunction()
+        //{
+        //    try
+        //    {
+        //        string targetDir = this.Context.Parameters["targetDir"];
+        //        string dll = Path.Combine(targetDir, "MyTry_NetOfficeAddin.dll");
+        //        // add codebase value
+        //        Assembly thisAssembly = Assembly.LoadFile(dll);
+        //        Type type = thisAssembly.GetType();
+        //        RegistryKey key = Registry.ClassesRoot.CreateSubKey("CLSID\\{" + type.GUID.ToString().ToUpper() + "}\\InprocServer32\\1.0.0.0");
+        //        key.SetValue("CodeBase", thisAssembly.CodeBase);
+        //        key.Close();
+
+        //        Registry.ClassesRoot.CreateSubKey(@"CLSID\{" + type.GUID.ToString().ToUpper() + @"}\Programmable");
+
+        //        // add bypass key
+        //        // http://support.microsoft.com/kb/948461
+        //        key = Registry.ClassesRoot.CreateSubKey("Interface\\{000C0601-0000-0000-C000-000000000046}");
+        //        string defaultValue = key.GetValue("") as string;
+        //        if (null == defaultValue)
+        //            key.SetValue("", "Office .NET Framework Lockback Bypass Key");
+        //        key.Close();
+
+        //        // register addin in Excel
+        //        RegistryKey regKeyExcel = Registry.CurrentUser.CreateSubKey(_addinOfficeRegistryKey + _progId);
+        //        regKeyExcel.SetValue("LoadBehavior", Convert.ToInt32(3));
+        //        regKeyExcel.SetValue("FriendlyName", _addinFriendlyName);
+        //        regKeyExcel.SetValue("Description", _addinDescription);
+        //        regKeyExcel.Close();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        string details = string.Format("{1}{1}Details:{1}{1}{0}", ex.Message, Environment.NewLine);
+        //        MessageBox.Show("An error occured." + details, "Register " + _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        //public  void UnregisterFunction()
+        //{
+        //    try
+        //    {
+        //        string targetDir = this.Context.Parameters["targetDir"];
+        //        string dll = Path.Combine(targetDir, "MyTry_NetOfficeAddin.dll");
+        //        Assembly thisAssembly = Assembly.LoadFile(dll);
+        //        Type type = thisAssembly.GetType();
+        //        // unregister addin
+        //        Registry.ClassesRoot.DeleteSubKey(@"CLSID\{" + type.GUID.ToString().ToUpper() + @"}\Programmable", false);
+
+        //        // unregister addin in office
+        //        Registry.CurrentUser.DeleteSubKey(_addinOfficeRegistryKey + _progId, false);
+
+        //    }
+        //    catch (Exception throwedException)
+        //    {
+        //        string details = string.Format("{1}{1}Details:{1}{1}{0}", throwedException.Message, Environment.NewLine);
+        //        MessageBox.Show("An error occured." + details, "Unregister" + _progId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        #endregion
+    }
+
+}
