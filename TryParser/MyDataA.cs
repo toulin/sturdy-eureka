7198fa52d2c414b23587dd424c9b1f83f4087ab7
toulin <toulin@cmoney.com.tw>
2022-09-19 15:32:01 +0800

    研究 動態Parser實值型別的方式與效率

---
+++ TryParser/MyDataA.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParser
+{
+    public class MyDataA
+    {
+        public long ID { get; set; }
+        public decimal Value { get; set; }
+        public double Value2 { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public decimal? NullableValue { get; set; }
+
+        public string Name { get; set; }
+
+
+        private T Parse<T>(object value)
+        {
+            if (value == null) return default(T);
+            var converter = TypeDescriptor.GetConverter(type: typeof(T));
+            return (T)converter.ConvertFrom(value); 
+        }
+
+        /// <summary>
+        /// 指定屬性名，寫入值
+        /// </summary>
+        /// <param name="values">key 屬性名 , value 值</param>
+        public void SetValue(Dictionary<string, string> values)
+        {
+            foreach(string key in values.Keys)
+            {
+                var p = this.GetType().GetProperty(key);
+                var value = values[key];
+                var converter = TypeDescriptor.GetConverter(p.PropertyType);
+                var result = converter.ConvertFrom(value);
+                p.SetValue(this, result);
+            }
+        }
+
+        /// <summary>
+        /// 指定屬性名，寫入值
+        /// </summary>
+        /// <param name="values">key 屬性名 , value 值</param>
+        public void SetValue2(Dictionary<string, string> values)
+        {
+            foreach (string key in values.Keys)
+            {
+                var p = this.GetType().GetProperty(key);
+                var value = values[key];
+                var nullable = Nullable.GetUnderlyingType(p.PropertyType);
+                if (nullable != null)
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        p.SetValue(this, null);
+                    }
+                    else
+                    {
+                        SetStringValue(p, nullable.BaseType , value);
+                    }
+                } 
+                else
+                {
+                    SetStringValue(p, p.PropertyType, value);
+                } 
+            }
+        }
+
+        private void SetStringValue(PropertyInfo propertyInfo, Type type, string value)
+        {
+            if (type.Equals(typeof(string)))
+            {
+                propertyInfo.SetValue(this, value);
+            }
+            else if (type.Equals(typeof(int)))
+            {
+                if (int.TryParse(value, out int valueInt))
+                {
+                    propertyInfo.SetValue(this, valueInt);
+                }
+            }
+            else if (type.Equals(typeof(long)))
+            {
+                if (long.TryParse(value, out long valueLong))
+                {
+                    propertyInfo.SetValue(this, valueLong);
+                }
+            }
+            else if (type.Equals(typeof(decimal)))
+            {
+                if (decimal.TryParse(value, out decimal valuedecimal))
+                {
+                    propertyInfo.SetValue(this, valuedecimal);
+                }
+            }
+            else if (type.Equals(typeof(double)))
+            {
+                if (double.TryParse(value, out double valuedouble))
+                {
+                    propertyInfo.SetValue(this, valuedouble);
+                }
+            }
+            else if (type.Equals(typeof(DateTime)))
+            {
+                if (DateTime.TryParse(value, out DateTime time))
+                {
+                    propertyInfo.SetValue(this, time);
+                }
+            }
+            else
+            {
+                propertyInfo.SetValue(this, value);
+            }
+        }
+    }
+}
---
+++ TryParser/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TryParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TEST();
+            TestA();
+        }
+        private static void TestA()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string s = "100";
+            int parserI;
+            for (int i = 0; i < 100000; i++)
+            {
+                parserI = int.Parse(s);
+            }
+            Console.WriteLine($"int.Parse spend {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                parserI = Parse<int>(s);
+            }
+            Console.WriteLine($"int.Parse spend {stopwatch.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 效率不如 指定的轉換(如int.Parser
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static  T Parse<T>(object value)
+        {
+            if (value == null) return default(T);
+            var converter = TypeDescriptor.GetConverter(type: typeof(T));
+            return (T)converter.ConvertFrom(value);
+        }
+
+        private static MyDataA CreateData()
+        {
+            MyDataA a = new MyDataA
+            {
+                ID = 1,
+                CreateTime = DateTime.Now,
+                Name = "t1",
+                NullableValue = 11,
+                Value = 222,
+                Value2 = 1.1
+            };
+            return a;
+        }
+        private static void TEST()
+        {
+            MyDataA A = CreateData();
+            MyDataA B = CreateData();
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("ID", "3");
+            values.Add("CreateTime", "2022/9/9 11:33:11");
+            values.Add("Name", "TEST Name");
+            values.Add("NullableValue", "");
+            values.Add("Value", "3.3");
+            values.Add("Value2", "4.3");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                A.SetValue(values);
+            }
+            Console.WriteLine($"TypeConvert spend {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            
+            for (int i = 0; i < 100000; i++)
+            {
+                B.SetValue2(values);
+            }
+            Console.WriteLine($"if else spend {stopwatch.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+    }
+}
