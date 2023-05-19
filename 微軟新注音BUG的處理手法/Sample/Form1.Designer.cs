369d3c5ecfd77363fcf71cfb56175ade6cf31f24
toulin <toulin@cmoney.com.tw>
2023-05-19 09:36:54 +0800

    加入版控-  微軟新注音BUG的處理手法 * 第一版 示例 問題，可用新、舊注音 在輸入框進行輸入與選字行為，可以看出差異 在KeyUp事件處理，原本預期是要移動下拉選單行為， 在使用舊注音(或其他輸入法)，在選字時是正常選字，不會影響下拉選單 但在 新注音，則會因為 傳送 KeyDown而無法判定"它正在選字"，而造成異常。 注：新注音在KeyUp事件中，其他參數也未見"可辦識選字狀態"的屬性。

---
+++ 微軟新注音BUG的處理手法/Sample/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.popupContainerControl1 = new DevExpress.XtraEditors.PopupContainerControl();
+            this.popupContainerEdit1 = new DevExpress.XtraEditors.PopupContainerEdit();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerControl1)).BeginInit();
+            this.popupContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // popupContainerControl1
+            // 
+            this.popupContainerControl1.Controls.Add(this.listBox1);
+            this.popupContainerControl1.Location = new System.Drawing.Point(92, 101);
+            this.popupContainerControl1.Name = "popupContainerControl1";
+            this.popupContainerControl1.Size = new System.Drawing.Size(508, 193);
+            this.popupContainerControl1.TabIndex = 0;
+            // 
+            // popupContainerEdit1
+            // 
+            this.popupContainerEdit1.EditValue = "";
+            this.popupContainerEdit1.Location = new System.Drawing.Point(92, 60);
+            this.popupContainerEdit1.Name = "popupContainerEdit1";
+            this.popupContainerEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.popupContainerEdit1.Properties.PopupControl = this.popupContainerControl1;
+            this.popupContainerEdit1.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.Standard;
+            this.popupContainerEdit1.Size = new System.Drawing.Size(443, 20);
+            this.popupContainerEdit1.TabIndex = 1;
+            this.popupContainerEdit1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.popupContainerEdit1_KeyUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(25, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(433, 144);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.popupContainerEdit1);
+            this.Controls.Add(this.popupContainerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerControl1)).EndInit();
+            this.popupContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PopupContainerControl popupContainerControl1;
+        private System.Windows.Forms.ListBox listBox1;
+        private DevExpress.XtraEditors.PopupContainerEdit popupContainerEdit1;
+    }
+}
+
---
+++ 微軟新注音BUG的處理手法/Sample/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void popupContainerEdit1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    if (!popupContainerEdit1.IsPopupOpen)
+                    {
+                        popupContainerEdit1.ShowPopup();
+                    }
+                    ListBoxDown();
+                    break;
+            }
+        }
+
+        private void ListBoxDown()
+        {
+            if (listBox1.SelectedIndex < listBox1.Items.Count)
+            {
+                listBox1.SelectedIndex += 1;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 99; i++)
+            {
+                listBox1.Items.Add($"List-{i}");
+            }
+        }
+    }
+}
