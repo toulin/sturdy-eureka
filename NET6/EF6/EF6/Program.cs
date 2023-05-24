41645119a8973372963c740182e57a51fcd8756e
toulin <toulin@cmoney.com.tw>
2023-05-24 10:34:41 +0800

    feat 增加另一種更新方式，以批次更新作為範例

--- NET6/EF6/EF6/Program.cs
+++ NET6/EF6/EF6/Program.cs
@@ -7,19 +7,20 @@ namespace EF6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("讀取最新帳號編號");
             int lastestAccountID;
-            //Read
+            //Read la
             using (var context = new MyDbContext())
             {
                 var account = context.Account.OrderByDescending(x => x.AccountID).FirstOrDefault();
                 lastestAccountID = account.AccountID;
                 Console.WriteLine($"Id: {account.AccountID}, Name: {account.Name}, Age: {account.Age}");
             }
-            Console.ReadLine();
 
-            //write 
+
+            Console.WriteLine($"即將新增一筆 ID={lastestAccountID+1}的資料");
+            Console.ReadLine();
+            //AddNew
             using (var context = new MyDbContext())
             {
                 var account = new Account()
@@ -47,6 +48,16 @@ namespace EF6
             }
             Console.WriteLine("更新完畢");
             Console.ReadLine();
+
+            //批次更新
+            using (var context = new MyDbContext())
+            {
+                var whereAccount = context.Account.Where(o => o.AccountID > 10);
+                whereAccount.ToList().ForEach(o => o.Name = $"Test-{o.Age}");
+                context.SaveChanges();
+            }
+            Console.ReadLine();
+            Console.WriteLine("批次更新完成");
         }
     }
 }
