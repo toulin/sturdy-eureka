cf21e21ebb6d723045e06d8663c771dff93aca87
toulin <toulin@cmoney.com.tw>
2023-10-13 17:57:57 +0800

    add 增加 Serilog測試

---
+++ SerilogTry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Configuration;
+
+namespace SerilogTry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            logger.Information("Hi..." );
+            Console.ReadLine();
+        }
+    }
+}
