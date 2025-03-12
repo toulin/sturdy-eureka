247ca7a47f76ad56fd7390ab9bac980ce02155be
toulin <toulin@cmoney.com.tw>
2025-03-12 16:18:39 +0800

    測試 Task.Run 執行緒怎麼跑

---
+++ TestTaskRun/TestTaskRun/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskRun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                Console.WriteLine("A");
+                await Test1();
+                Console.WriteLine("B");
+
+                await Test2();
+                Console.WriteLine("C");
+            });
+            Console.WriteLine("D");
+            Console.ReadLine();
+        }
+
+
+        static Task Test1()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Test1 1");
+                Task.Delay(1000).Wait();
+                Console.WriteLine("Test1 2");
+            });
+        }
+
+        static Task Test2()
+        {
+            return Task.Run(async() =>
+            {
+                Console.WriteLine("Test2 1");
+                await Task.Delay(1000);
+                Console.WriteLine("Test2 2");
+            });
+        }
+    }
+}
